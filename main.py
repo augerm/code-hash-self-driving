b3886f74b1d745dcfe1867dc05e93ede5055454f
Michael Auger <michaelauger@Michaels-MBP.attlocal.net>
2019-02-24 12:22:19 -0600

    Convert header to variables

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ row_cols = ['x1', 'y1', 'x2', 'y2', 'earliest_start', 'latest_finish']
 
 data = read_file(os.path.join('data', 'a_example.in'), header_cols, row_cols)
 
-rides = []
+rows = data['header']['rows']
+cols = data['header']['cols']
+num_vehicles = data['header']['num_vehicles']
+num_rides = data['header']['num_rides']
+bonus = data['header']['bonus']
+steps = data['header']['steps']
+
+rides = data['rows']
+
 vehicles = []
